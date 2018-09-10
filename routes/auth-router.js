62bbca3a37028111745d4e0e31b38586416436f7
Constance Reversé <constance.reverse@hotmail.fr>
2018-09-10 11:32:00 +0200

    done

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -10,5 +10,21 @@ router.get("/signup", (req, res, next) => {
   res.render("auth-views/signup-form.hbs");
 });
 
+router.post("/process-signup", (req, res, next) => {
+  const { fullName, email, originalPassword } = req.body;
+
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+
+  User.create({ fullName, email, encryptedPassword })
+    .then(userDoc => { 
+      req.flash("success", "Sign up success! 👏");
+      res.redirect("/")
+    })
+    .catch(err => next(err));
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth-views/login-form.hbs");
+});
 
 module.exports = router;
