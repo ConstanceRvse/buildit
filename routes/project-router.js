6e2eb2a64a209799c15a23fa0640002144b839ef
Constance Reversé <constance.reverse@hotmail.fr>
2018-09-10 14:06:22 +0200

    project-router

---
+++ routes/project-router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const Project = require("../models/project-model.js");
+
+const User = require("../models/user-model.js");
+
+const router = express.Router();
+
+
+
+
+module.exports = router;
